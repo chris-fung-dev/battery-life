9193a38d505d39b49d9b0a8521f3ba812b1361be
chris-fung-dev <115664914+chris-fung-dev@users.noreply.github.com>
2023-11-26 11:50:41 -0500

    Rest (#24)

--- Assets/Scenes/Scripts/PlayerMovement.cs
+++ Assets/Scenes/Scripts/PlayerMovement.cs
@@ -1,74 +1,87 @@
 using System;
 using UnityEngine;
 
-namespace Scenes.Scripts
+public class PlayerMovement : MonoBehaviour
 {
-    public class PlayerMovement : MonoBehaviour
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpy;
+    private Rigidbody2D _body;
+    private bool _isJumping;
+    public Animator anim;
+    public AudioSource JumpSound;
+    public int SoundToken;
+
+    private SpriteRenderer spriteRenderer; // Added SpriteRenderer variable
+    private bool facingRight = true; // Indicates whether the player is facing right
+
+    private void Awake()
     {
-        [SerializeField] private float speed;
-        [SerializeField] private float jumpy;
-        private Rigidbody2D _body;
-        private bool _isJumping;
-        public Animator anim;
-        public AudioSource JumpSound;
-        public int SoundToken;
+        _body = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>(); // Get the SpriteRenderer component
+    }
 
-        private void Awake()
+    private void Update()
+    {
+        _body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, _body.velocity.y);
 
+        float move = Input.GetAxis("Horizontal");
+
+        // Flip the sprite based on movement direction
+        if (move > 0 && !facingRight)
         {
-            _body = GetComponent<Rigidbody2D>();
+            FlipSprite();
+        }
+        else if (move < 0 && facingRight)
+        {
+            FlipSprite();
         }
 
-        private void Update()
-        {
-            _body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, _body.velocity.y);
-         
-            
-
-            
-
-            if (Input.GetKey(KeyCode.Space) && !_isJumping)
-            { 
-                _body.velocity = new Vector2(_body.velocity.x,jumpy);
-                _isJumping = true;
-            }
-
-            if (_isJumping)
-            {
-                anim.SetBool("Jump", true);
-                if (SoundToken == 1)
-                {
-                    JumpSound.Play();
-                }
-                SoundToken =- 1;
-            }
+        if (Input.GetKey(KeyCode.Space) && !_isJumping)
+        { 
+            _body.velocity = new Vector2(_body.velocity.x, jumpy);
+            _isJumping = true;
         }
 
-        private void FixedUpdate()
+        if (_isJumping)
         {
-            float move = Input.GetAxis("Horizontal");
-
-            // Check if the player is moving horizontally
-            if (Mathf.Abs(move) > 0)
+            anim.SetBool("Jump", true);
+            if (SoundToken == 1)
             {
-                anim.SetBool("Run", true);
+                JumpSound.Play();
             }
-            else
-            {
-                anim.SetBool("Run", false);
-            }
+            SoundToken = -1;
         }
+    }
 
+    private void FixedUpdate()
+    {
+        float move = Input.GetAxis("Horizontal");
 
-        private void OnCollisionEnter2D(Collision2D other)
+        // Check if the player is moving horizontally
+        if (Mathf.Abs(move) > 0)
         {
-            if (other.gameObject.CompareTag("Ground"))
-            {
-                _isJumping = false;
-                anim.SetBool("Jump", false);
-                SoundToken =+ 1;
-            }
-            
+            anim.SetBool("Run", true);
         }
+        else
+        {
+            anim.SetBool("Run", false);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            _isJumping = false;
+            anim.SetBool("Jump", false);
+            SoundToken = +1;
+        }
+    }
+
+    // Function to flip the player's sprite horizontally
+    private void FlipSprite()
+    {
+        facingRight = !facingRight; // Toggle facingRight
+        spriteRenderer.flipX = !spriteRenderer.flipX; // Flip the sprite
     }
 }
