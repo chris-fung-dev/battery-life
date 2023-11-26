4cd857112ad6c78d3fefd0f97e87cf1ed4df4c43
chris-fung-dev <115664914+chris-fung-dev@users.noreply.github.com>
2023-11-26 00:05:53 -0500

    Level1 (#17)

---
+++ Assets/NextLevel.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevel : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
